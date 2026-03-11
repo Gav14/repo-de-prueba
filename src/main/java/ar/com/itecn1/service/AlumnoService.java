f76f97ce0049adb2f13154c8e9b957c6c53d65e6
Gav14 <maurogavilan8@gmail.com>
2026-03-11 19:56:26 -0300

    Update AlumnoService.java

--- src/main/java/ar/com/itecn1/service/AlumnoService.java
+++ src/main/java/ar/com/itecn1/service/AlumnoService.java
@@ -6,9 +6,5 @@ import java.util.List;
 
 public interface AlumnoService extends CRUDService<Alumno> {
     Alumno findByDni(String dni);
-    List<Alumno> findAll();
-    void save(Alumno alumno);
-    void update(Alumno alumno);
-    void delete(Alumno alumno);
     boolean validarFormatoDni(String dni);
 }

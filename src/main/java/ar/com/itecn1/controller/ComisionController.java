91bf64ed11319823d040baee2814c9bcce6d7e6d
Julio Argentino Cuello <juliofranco0607@gmail.com>
2026-03-12 18:35:08 -0300

    correccion temporal comision

--- src/main/java/ar/com/itecn1/controller/ComisionController.java
+++ src/main/java/ar/com/itecn1/controller/ComisionController.java
@@ -1,9 +1,10 @@
 package ar.com.itecn1.controller;
 
 import ar.com.itecn1.model.ComisionMateria;
+import ar.com.itecn1.model.AlumnoInscriptoMateria;
+import ar.com.itecn1.model.Asistencia;
 import ar.com.itecn1.service.ComisionService;
 import ar.com.itecn1.service.impl.ComisionServiceImpl;
-
 import java.util.List;
 
 public class ComisionController {
@@ -13,23 +14,51 @@ public class ComisionController {
         this.comisionService = new ComisionServiceImpl();
     }
 
-    public List<ComisionMateria> findAll(){
-        return this.comisionService.findAll();
+    public List<ComisionMateria> findAll() {
+        return comisionService.findAll();
     }
 
-    public ComisionMateria findByCode(String codigoMateria){
-        return this.comisionService.findByCode(codigoMateria);
+    public ComisionMateria findByCode(String codigoComision) {
+        return comisionService.findByCode(codigoComision);
     }
 
-    public void createComision(ComisionMateria comisionMateria){
-        this.comisionService.save(comisionMateria);
+    public List<ComisionMateria> findByCarrera(String nombreCarrera) {
+        return comisionService.findByCarrera(nombreCarrera);
     }
 
-    public void updateComsion(ComisionMateria comisionMateria){
-        this.comisionService.update(comisionMateria);
+    public List<ComisionMateria> findByMateria(String codigoMateria) {
+        return comisionService.findByMateria(codigoMateria);
     }
 
-    public void deleteComision(ComisionMateria comisionMateria){
-        this.comisionService.delete(comisionMateria);
+    public List<ComisionMateria> findByCuatrimestre(String numeroCuatrimestre) {
+        return comisionService.findByCuatrimestre(numeroCuatrimestre);
+    }
+
+    public List<ComisionMateria> findByProfesor(int dniProfesor) {
+        return comisionService.findByProfesor(dniProfesor);
+    }
+
+    public String registrarComision(ComisionMateria comision) {
+        return comisionService.registrarComision(comision);
+    }
+
+    public String inscribirAlumno(String codigoComision, String dniAlumno, AlumnoInscriptoMateria inscripcion) {
+        return comisionService.inscribirAlumno(codigoComision, dniAlumno, inscripcion);
+    }
+
+    public String registrarAsistencia(String codigoComision, String dniAlumno, Asistencia asistencia) {
+        return comisionService.registrarAsistencia(codigoComision, dniAlumno, asistencia);
+    }
+
+    public boolean puedeRegistrarAsistencia(String codigoComision, String dniAlumno) {
+        return comisionService.puedeRegistrarAsistencia(codigoComision, dniAlumno);
+    }
+
+    public void updateComision(ComisionMateria comision) {
+        comisionService.update(comision);
+    }
+
+    public void deleteComision(ComisionMateria comision) {
+        comisionService.delete(comision);
     }
 }
